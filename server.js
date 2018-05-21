d78429bf38534aaf7a2b9ed14b381408bbb497ad
Michael Gatewood <michael@mngatewood.com>
2018-05-21 11:23:58 -0600

    Adds post and delete endpoints

--- server.js
+++ server.js
@@ -21,6 +21,41 @@ app.get('/api/v1/photos', (request, response) => {
     })
 })
 
+app.post('/api/v1/photos/', (request, response) => {
+  const photo = request.body
+  const { title, url } = request.body;
+
+  if (title && url) {
+    database('photos').insert( photo, 'title' )
+      .then(title => response.status(201)
+        .json({ result: `${title} added successfully.` }))
+      .catch(error => {
+          response.status(500).json(error)
+      })
+  } else {
+    return response.status(422).json('Must provide photo title and url');
+  }
+});
+
+app.delete('/api/v1/photos/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('photos').where('id', id ).del()
+    .then(result => {
+      console.log(result)
+      if (result) {
+        return response
+          .status(200)
+          .json({ result: `Photo ${id} deleted successfully.` })
+      } else {
+        return response
+          .status(404)
+          .send({ error: `Photo ${id} not found.` })
+      }
+    })
+    .catch(error => response.status(404).json({ error }));
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} listening on localhost:${app.get('port')}.`); // eslint-disable-line
 });
