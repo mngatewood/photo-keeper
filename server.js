9a975d0896b317e32f10362d408ae7785759672a
Michael Gatewood <michael@mngatewood.com>
2018-05-21 10:37:01 -0600

    Adds seed to database

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+
+
+
+module.exports = { app, database };
